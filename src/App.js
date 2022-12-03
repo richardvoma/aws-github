8257dc5de0782b19a617b62ba8256de6bcf928e7
richardvoma <richardvoma1@gmail.com>
2022-12-03 12:22:11 -0500

    production

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ function App() {
     <div className="App">
      <h1>Welcome to compudemy</h1>
      please how can i help you,please let us know if you need any help
-     please subscribe and keep following us as we solve your problem.
+     please subscribe and keep following us as we solve your problem
     </div>
   );
 }
