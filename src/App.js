76a7a8c38296de8a53d4110a3b1e691b2050c587
richardvoma <richardvoma1@gmail.com>
2022-12-03 12:31:40 -0500

    production

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
      <h1>Welcome to compudemy</h1>
      please how can i help you,please let us know if you need any help
      please subscribe and keep following us as we solve your problem
+     you can follow us on you tube.
     </div>
   );
 }
